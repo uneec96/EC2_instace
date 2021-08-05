602cd95245f816108cfea20766e820f0bbd829fb
uneec96 <86100199+uneec96@users.noreply.github.com>
2021-08-05 22:16:06 +0530

    Update instances.tf

--- ec2/instances.tf
+++ ec2/instances.tf
@@ -2,10 +2,6 @@ resource "aws_instance" "web1" {
   count          = var.instance_count
   ami            = var.ami
   instance_type  = var.instance_type
-
-  tags = {
-      Name  = element(var.instance_tags, count.index)
-    }
 }
 
 resource "aws_ebs_volume" "example" {
@@ -17,14 +13,4 @@ resource "aws_volume_attachment" "ebs_att1" {
   device_name = "/dev/sdh"
   volume_id   = "${aws_ebs_volume.example.id}"
   instance_id = "${aws_instance.web1[0].id}"
-}
-resource "aws_volume_attachment" "ebs_att2" {
-  device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.example.id}"
-  instance_id = "${aws_instance.web1[1].id}"
-}
-resource "aws_volume_attachment" "ebs_att3" {
-  device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.example.id}"
-  instance_id = "${aws_instance.web1[2].id}"
 }
