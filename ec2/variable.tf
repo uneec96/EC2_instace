37c84f70c1e8bc727a254fdad692ddadb86b3554
uneec96 <86100199+uneec96@users.noreply.github.com>
2021-08-05 22:16:45 +0530

    Update variable.tf

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,16 +1,10 @@
 variable "ami" {
     default = "ami-00bf4ae5a7909786c"
 }
-
-variable "instance_tags" {
-  type = list
-  default = ["Terraform-1", "Terraform-2"]
-}
-
 variable "instance_type" {
     default = "t2.micro"  
 }
 
 variable "instance_count" {
-  default = "3"
+  default = "1"
 }
