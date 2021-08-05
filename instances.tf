4720116cda3c8da063be93e61ce6f97f05bdeded
uneec96 <86100199+uneec96@users.noreply.github.com>
2021-08-05 22:22:16 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,1 +1,17 @@
 
+resource "aws_instance" "web1" {
+  count          = var.instance_count
+  ami            = var.ami
+  instance_type  = var.instance_type
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-south-1a"
+  size              = 8
+}
+
+resource "aws_volume_attachment" "ebs_att1" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.example.id}"
+  instance_id = "${aws_instance.web1[0].id}"
+}
